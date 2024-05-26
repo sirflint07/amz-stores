70f1dbeba75904a8a603b66099d223a9a196234d
Michael Flint <104478810+sirflint07@users.noreply.github.com>
2024-05-26 01:25:07 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,1 @@
 export const round = (num: number) => Math.round(((num + Number.EPSILON) * 100) / 100)
-
-
-// mongodb+srv://fajobimichael123:Oluwaseun2001.@cluster0.pc6gmll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// Oluwaseun2001.
-//
